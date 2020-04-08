0a5261a40c84405859181d02d6f1c6f97f3573d1
Alessioch <alessiochf@gmail.com>
2020-04-08 23:25:10 +0100

    added connectivity check

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+import { NetInfo } from 'react-native';
 
 export default class MyWeb extends Component {
+  state = {
+    isConnected: true
+  };
+
+  componentDidMount() {
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = isConnected => {
+      this.setState({ isConnected });
+  };
+
   render() {
+    if (!this.state.isConnected) {
+      return 'search for internet';
+    }
     return (
       <WebView
         source={{ uri: 'https://corporate.arcelormittal.com/investors' }}
